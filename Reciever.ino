b0a3769959b6346679454b0978479fdd8bf2385f
Sagnik Chatterjee <97253300+sagn-ik@users.noreply.github.com>
2025-07-01 08:31:58 +0530

    Update Reciever.ino

--- Reciever.ino
+++ Reciever.ino
@@ -1,20 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+
 #define RELAY D4
-// Structure example to receive data
-// Must match the sender structure
+
 typedef struct struct_message {
-    char a[32];
     int b;
-    float c;
-    String d;
     bool e;
 } struct_message;
 
-// Create a struct_message called myData
+
 struct_message myData;
 
-// Callback function that will be executed when data is received
+
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&myData, incomingData, sizeof(myData));
   Serial.print("Bytes received: ");
@@ -29,24 +26,18 @@ void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
 }
  
 void setup() {
-  // Initialize Serial Monitor
   Serial.begin(115200);
   pinMode(RELAY,OUTPUT);
-  // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
   if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
   
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
   esp_now_register_recv_cb(OnDataRecv);
 }
 
 void loop() {
-  
 }
